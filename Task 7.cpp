a61272a79744e034e24e0bdd0032bd23cb67266e
Ikrash Bakhsh <153372614+Ikrash-Bakhsh@users.noreply.github.com>
2024-01-13 21:55:36 +0500

    Add files via upload

---
+++ Task 7.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the number to find cube of it = ";
+	cin>>a;
+	b=a*a*a;
+	cout<<"Cube of number = "<<b;
+	return 0;
+}
